46b83a1429470086a686f582b12ccd76383ca3ab
Vitalii Mural <vitalii.mural@gmail.com>
2024-01-09 17:57:43 +0200

    v1.0.0.54

--- src/Models/SipAccount.cs
+++ src/Models/SipAccount.cs
@@ -1,9 +1,18 @@
-﻿namespace Qexal.CTI.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Qexal.CTI.Models;
 
 public class ConfigurationDto
 {
+    [JsonPropertyName("internalNumber")]
     public int InternalNumber { get; set; }
+
+    [JsonPropertyName("displayName")]
     public string DisplayName { get; set; } = null!;
+
+    [JsonPropertyName("settings")]
     public string Settings { get; set; } = null!;
+
+    [JsonPropertyName("dateTime")]
     public DateTime DateTime { get; set; }
 }
